b93027465515c1942fe40bc7f74de5459ad258d0
moeKiwiSAMA <me@kiwi.cat>
2021-09-23 14:52:39 +0800

    pong

--- src/main/kotlin/cat/kiwi/simple/Application.kt
+++ src/main/kotlin/cat/kiwi/simple/Application.kt
@@ -25,6 +25,7 @@ class Application {
             }
 
             simpleRouter.get("/ping") { ctx ->
+                ctx.end("pong")
             }
             simpleRouter.get("/foo") { ctx ->
                 ctx.badRequest()
